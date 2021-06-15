45581e6ab31f9bbc48f4bd2a600725b8cf86dc6b
中瑋 蔡 <107111248@mail.oit.edu.tw>
2021-06-15 16:36:51 +0800

    暫定

---
+++ Team01/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Team01.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" ></asp:TextBox><br />
+            <asp:TextBox ID="TextBox2" runat="server" Visible="False"></asp:TextBox><br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" /><br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Team01/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Team01
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) { 
+                Random rm = new Random();
+            TextBox2.Text = rm.Next(1, 100).ToString();
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+                if (int.Parse(TextBox1.Text) < 1 & int.Parse(TextBox1.Text) > 100)
+                {
+                    Label1.Text = "請輸入1~100的數值";
+                }
+
+
+            if (int.Parse(TextBox1.Text) == int.Parse(TextBox2.Text))
+                {
+                Label1.Text = "正確";
+                }
+                else if (int.Parse(TextBox1.Text) > int.Parse(TextBox2.Text))
+                {
+                Label1.Text = "在小一點";
+            }
+                else if (int.Parse(TextBox1.Text) < int.Parse(TextBox2.Text))
+                {
+                Label1.Text = "在大一點";
+                 }
+         }
+    }
+ }
